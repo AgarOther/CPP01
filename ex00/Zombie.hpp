c59a941079b8e051617d7adf0c0f12d6d54d3363
AgarOther <agarother@gmail.com>
2024-12-20 16:14:20 +0100

    Templates

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scraeyme <scraeyme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 16:07:28 by scraeyme          #+#    #+#             */
+/*   Updated: 2024/12/20 16:12:00 by scraeyme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		Zombie(void);
+		~Zombie(void);
+		void	announce(void);
+};
+
+#endif
